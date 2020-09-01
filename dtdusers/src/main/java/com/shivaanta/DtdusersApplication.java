7ff156afc66bad3f80ae28c13c28e71437a72e7f
anil503rgpv <36809011+anil503rgpv@users.noreply.github.com>
2020-09-01 22:50:53 +0530

    initial commit for users service

---
+++ dtdusers/src/main/java/com/shivaanta/DtdusersApplication.java
@@ -0,0 +1,13 @@
+package com.shivaanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtdusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtdusersApplication.class, args);
+	}
+
+}
---
+++ dtdusers/src/test/java/com/shivaanta/DtdusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivaanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtdusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
